d565eb10b0b03cd64e4ac0686599d683e269d1ed
adityakotambeti <80984795+adityakotambeti@users.noreply.github.com>
2021-07-12 17:01:26 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -3,27 +3,22 @@ import './App.css';
 import Routes from './routes/index';
 import styled,{ ThemeProvider } from 'styled-components';
 import { dark,light } from './theme';
-import ToggleButton from './pages/ToggleComponent/ToggleButton';
+import ToggleButton from './components/ToggleButton';
 const StyledApp = styled.div`
   color: ${(props) => props.theme.fontColor};
   background-color: ${(props) => props.theme.backgroundColor};
   `;
 function App() {
   const [themeValue,setThemeValue] = useState('true')
-  const sendDataToParent = (status) => { // the callback. Use a better name
-    console.log("main",status);
-    setThemeValue(status)
-     
-  };
+  const sendDataToParent = (status) => { setThemeValue(status); }
 
  
   return (
     <>
-      <ThemeProvider  theme={themeValue?light:dark}>
-
+      <ThemeProvider  theme={themeValue ? light : dark}>
         <StyledApp>
         <ToggleButton className="togglebtn" sendDataToParent={sendDataToParent} />
-         <Routes  />
+         <Routes />
         </StyledApp>
       </ThemeProvider>
     </>
